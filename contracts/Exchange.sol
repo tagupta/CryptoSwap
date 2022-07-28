30539f893cf235db6f48208be307178fedab4427
tagupta <guptatanukhandelwal.96@gmail.com>
2022-07-28 15:35:56 +0530

    fix: addLiquidity

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,19 +8,105 @@ contract Exchange {
     address public tokenAddress;
 
     error InvalidTokenAddress();
+    error InvalidReserves();
+    error InsufficientEth();
+    error InsufficientTokens();
+    error IncorrectOutputAmount();
+    error EthTransferFailed();
 
     constructor(address _token) {
         if (_token == address(0)) revert InvalidTokenAddress();
         tokenAddress = _token;
     }
 
-    function addLiquidity(uint256 _tokenAmount) public payable {
-        IERC20 token = IERC20(tokenAddress);
-        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    function addLiquidity(uint256 maxtokenAmount) public payable {
+        if (getReserve() == 0) {
+            IERC20 token = IERC20(tokenAddress);
+            token.transferFrom(msg.sender, address(this), maxtokenAmount);
+        } else {
+            uint256 ethReserve = address(this).balance - msg.value;
+            uint256 tokenReserve = getReserve();
+            uint256 tokenAmount = (msg.value * tokenReserve) / ethReserve;
+            if (maxtokenAmount < tokenAmount) revert InsufficientTokens();
+            IERC20(tokenAddress).transferFrom(
+                msg.sender,
+                address(this),
+                tokenAmount
+            );
+        }
     }
 
     //Helper function for getting token balance of an exchange
     function getReserve() public view returns (uint256 balance) {
         balance = IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    //naive function for fetching prices
+    function getPrices(uint256 inputReserve, uint256 outputReserve)
+        public
+        pure
+        returns (uint256)
+    {
+        if (inputReserve <= 0 || outputReserve <= 0) revert InvalidReserves();
+        return (inputReserve * 1000) / outputReserve;
+    }
+
+    //Computing output amount
+    function getAmounts(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
+        if (inputReserve <= 0 || outputReserve <= 0) revert InvalidReserves();
+        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    //get output amount for exact ETHs
+    function getTokenAmount(uint256 _ethSold)
+        public
+        view
+        returns (uint256 tokenBought)
+    {
+        if (_ethSold <= 0) revert InsufficientEth();
+        uint256 tokenReserve = getReserve();
+        tokenBought = getAmounts(_ethSold, address(this).balance, tokenReserve);
+    }
+
+    //get output amount for exact tokens
+    function getEthAmount(uint256 _tokenSold)
+        public
+        view
+        returns (uint256 ethBought)
+    {
+        if (_tokenSold <= 0) revert InsufficientTokens();
+        uint256 tokenReserve = getReserve();
+        return getAmounts(_tokenSold, tokenReserve, address(this).balance);
+    }
+
+    //Eth to token swap [Exact Eth, min tokens]
+    function ethToTokenSwap(uint256 minTokens)
+        public
+        payable
+        returns (uint256 tokensBought)
+    {
+        uint256 inputReserve = address(this).balance - msg.value;
+        uint256 outputReserve = getReserve();
+        tokensBought = getAmounts(msg.value, inputReserve, outputReserve);
+        if (tokensBought < minTokens) revert IncorrectOutputAmount();
+        IERC20(tokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    //Token to Eth swap [Exact token, min Eth]
+    function tokenToEthSwap(uint256 tokenSold, uint256 minEther)
+        public
+        payable
+        returns (uint256 ethBought)
+    {
+        uint256 inputReserve = getReserve();
+        uint256 outputReserve = address(this).balance;
+        ethBought = getAmounts(tokenSold, inputReserve, outputReserve);
+        if (minEther > ethBought) revert IncorrectOutputAmount();
+        (bool result, ) = payable(msg.sender).call{value: ethBought}("");
+        if (!result) revert EthTransferFailed();
     }
 }
--- test/testAddLiquidity.js
+++ test/testAddLiquidity.js
@@ -33,13 +33,61 @@ describe("Exchange", () => {
         expect(await exchange.deployed()).to.equal(exchange);
     });
 
-    describe("Add Liquidity", async () => {
-        it("should add liquidity", async () => {
-            await token.approve(exchange.address, toWei(200));
-            await exchange.addLiquidity(toWei(200), { value: toWei(100) });
+    // describe("Add Liquidity", async () => {
+    //     it("should add liquidity", async () => {
+    //         await token.approve(exchange.address, toWei(200));
+    //         await exchange.addLiquidity(toWei(200), { value: toWei(100) });
 
-            expect(await getBalance(exchange.address)).to.equal(toWei(100));
-            expect(await exchange.getReserve()).to.equal(toWei(200));
+    //         expect(await getBalance(exchange.address)).to.equal(toWei(100));
+    //         expect(await exchange.getReserve()).to.equal(toWei(200));
+    //     });
+    // });
+
+    describe("Get Price", async () => {
+        it("should return correct prices", async () => {
+            await token.approve(exchange.address, toWei(2000));
+            await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+
+            const ethReserve = await getBalance(exchange.address); //100
+            const tokenReserve = await exchange.getReserve(); //200
+
+            expect(await exchange.getPrices(tokenReserve, ethReserve)).to.equal(2000);
+            expect(await exchange.getPrices(ethReserve, tokenReserve)).to.equal(500);
         });
     });
+
+    describe("Get Token amount", async () => {
+        it("should return right token amount", async () => {
+            await token.approve(exchange.address, toWei(2000));
+            await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+
+            let tokensBought = await exchange.getTokenAmount(toWei(1)); //Passing in eth to be sold
+            expect(fromWei(tokensBought)).to.eq("1.998001998001998001");
+
+            tokensBought = await exchange.getTokenAmount(toWei(100));
+            expect(fromWei(tokensBought)).to.eq("181.818181818181818181");
+
+            tokensBought = await exchange.getTokenAmount(toWei(1000));
+            expect(fromWei(tokensBought)).to.eq("1000.0");
+
+        })
+    })
+
+    describe("Get Eth amount", async () => {
+        it("should return right eth amount", async () => {
+            await token.approve(exchange.address, toWei(2000));
+            await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+
+            let ethBought = await exchange.getEthAmount(toWei(2)) //Passing in tokens to be sold
+            expect(fromWei(ethBought)).to.eq("0.999000999000999");
+
+            ethBought = await exchange.getEthAmount(toWei(100)) //Passing in tokens to be sold
+            expect(fromWei(ethBought)).to.eq("47.619047619047619047");
+
+            ethBought = await exchange.getEthAmount(toWei(2000)) //Passing in tokens to be sold
+            expect(fromWei(ethBought)).to.eq("500.0");
+        })
+    })
+
+
 })
