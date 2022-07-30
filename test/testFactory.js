23b24ac0ea4d5077ff75d37605fb562f169f1413
tagupta <guptatanukhandelwal.96@gmail.com>
2022-07-30 18:24:15 +0530

    fix: test cases

--- test/testFactory.js
+++ test/testFactory.js
@@ -8,14 +8,22 @@ const fromWei = (value) =>
         typeof value === "string" ? value : value.toString()
     );
 
+const createExchange = async (factory, tokenAddress, sender) => {
+    var exchangeAddress = await factory.connect(sender).callStatic.createExchange(
+        tokenAddress
+    );
+    await factory.connect(sender).createExchange(tokenAddress);
+    const Exchange = await ethers.getContractFactory("Exchange");
+    return await Exchange.attach(exchangeAddress);
+}
+
 describe("Factory", () => {
     let owner;
     let user;
     let factory;
     let token1;
     let token2;
-    let exchangeAddress_1;
-    let exchangeAddress_2;
+
 
     beforeEach(async () => {
         [owner, user] = await ethers.getSigners();
@@ -34,54 +42,26 @@ describe("Factory", () => {
 
             token2 = await Token.deploy("Mango", "mango", toWei(10 ** 6));
             await token2.deployed();
-
-            console.log("token1::: ", token1.address);
-            console.log("token2::: ", token2.address);
-
-            exchangeAddress_1 = await factory.callStatic.createExchange(
-                token1.address
-            );
-            console.log("exchangeAddress1: ", exchangeAddress_1);
-            await factory.createExchange(token1.address);
-            expect(await factory.tokenToExchange(token1.address)).to.eq(exchangeAddress_1);
-
-            exchangeAddress_2 = await factory.callStatic.createExchange(
-                token2.address
-            );
-            console.log("exchangeAddress2: ", exchangeAddress_2);
-            await factory.createExchange(token2.address);
-            expect(await factory.tokenToExchange(token2.address)).to.eq(exchangeAddress_2);
         })
 
         it("Adding liquidity into both exchanges for trades", async () => {
 
-            const Exchange = await ethers.getContractFactory("Exchange");
-            const exchange_1 = await Exchange.attach(exchangeAddress_1);
-            const exchange_2 = await Exchange.attach(exchangeAddress_2);
-
-            console.log("Token 1 address: ", token1.address);
-            console.log("Token 2 address: ", token2.address);
-            console.log("Exchange 1 address: ", exchange_1.address);
-            console.log("Exchange 2 address: ", exchange_2.address);
+            const exchange_1 = await createExchange(factory, token1.address, owner);
+            const exchange_2 = await createExchange(factory, token2.address, owner);
 
             await token1.approve(exchange_1.address, toWei(2000));
             await exchange_1.addLiquidity(toWei(2000), { value: toWei(1000) });
 
-            await token2.approve(exchangeAddress_2, toWei(2000));
+            await token2.approve(exchange_2.address, toWei(2000));
             await exchange_2.addLiquidity(toWei(2000), { value: toWei(1000) });
 
             await token1.transfer(user.address, toWei(200));
-            await token1.connect(user).approve(exchangeAddress_1, toWei(200));
+            await token1.connect(user).approve(exchange_1.address, toWei(200));
 
             await exchange_1.connect(user).tokenToTokenSwap(toWei(200), toWei(1), token2.address);
 
-            const bal_Token1_User = await token1.balanceOf(user.address);
-            const bal_Token2_User = await token2.balanceOf(user.address);
-
-            console.log("Token 2 balance in reserve:", fromWei(await exchange_2.getReserve()))
-            console.log("bal_Token1_User: ", bal_Token1_User);
-            console.log("bal_Token2_User: ", fromWei(bal_Token2_User));
-
+            expect(fromWei(await token2.balanceOf(user.address))).to.eq("163.758030425810979022");
+            expect(fromWei(await exchange_2.getReserve())).to.eq("1836.241969574189020978");
         })
     })
 
